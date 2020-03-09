37a33e74062f85bb2176dd1b2406f25b8fd027f8
fanyao <fanyao931213>
2020-03-09 09:40:55 +0800

    文件上传到临时目录

--- src/main/java/com/yao/demo/fastdfs/FastDfsApplication.java
+++ src/main/java/com/yao/demo/fastdfs/FastDfsApplication.java
@@ -2,12 +2,39 @@ package com.yao.demo.fastdfs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 @SpringBootApplication
 public class FastDfsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FastDfsApplication.class, args);
+    }
+
+    // 上传文件到临时文件夹下  已天为文件夹分隔
+    SimpleDateFormat sdf = new SimpleDateFormat("/yyyy/MM/dd/");
+    @PostMapping("/import")
+    public String importData(MultipartFile file, HttpServletRequest req) throws IOException {
+        String format = sdf.format(new Date());
+        String realPath = req.getServletContext().getRealPath("/upload") + format;
+        File folder = new File(realPath);
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+        String oldName = file.getOriginalFilename();
+        String newName = UUID.randomUUID().toString() + oldName.substring(oldName.lastIndexOf("."));
+        file.transferTo(new File(folder,newName));
+        String url = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + "/upload" + format + newName;
+        System.out.println(url);
+        return "上传成功!";
     }
 
 }
